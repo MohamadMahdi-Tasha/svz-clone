757f8df186ff379b0d250386d0ab89014e0b00c4
Mahdi Tasha <imwhdiiii@gmail.com>
2023-09-23 11:12:51 +0330

    added placeholder home page file

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+// Codes By Mahdi Tasha
+// Importing Part
+import {ReactNode} from "react";
+
+// Creating And Exporting Home Page As Default
+
+export default function HomePage():ReactNode {
+    // Returning JSX
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    );
+}

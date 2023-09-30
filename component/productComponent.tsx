bc2824a2c2b268280a6c7cbbb39a6c94b0abf9ce
Mahdi Tasha <imwhdiiii@gmail.com>
2023-09-30 16:05:01 +0330

    break down second section to smalle components

---
+++ component/productComponent.tsx
@@ -0,0 +1,26 @@
+// Codes By Mahdi Tasha
+// Importing Part
+import {ReactNode} from "react";
+
+// Defining Type Of Props
+interface propsType {
+    title: string;
+    children: ReactNode;
+    icon: 'circle' | 'square';
+    iconColor: string;
+    willScale?: boolean;
+}
+
+// Creating And Exporting Product Component As Default
+export default function ProductComponent({iconColor,icon,children,title,willScale = false}:propsType):ReactNode {
+    // Returning JSX
+    return (
+        <a href={'#'} className={`border border-white/30 p-[30px] ${(willScale) ? ' xl:col-span-1 lg:col-span-3 cols-span-1' : ''}`}>
+            <div className={'mb-[200px]'}>
+                <h5 className={'text-white text-[32px] text-bold mb-[20px] truncate w-full'}>{title}</h5>
+                <p className={'text-white/80 text-[16px] text-light line-clamp-5'}>{children}</p>
+            </div>
+            <div className={`${iconColor} max-w-[100px] aspect-square${(icon === 'circle') ? ' rounded-full' : ''}`} />
+        </a>
+    );
+}
--- component/section/secondSectionComponent.tsx
+++ component/section/secondSectionComponent.tsx
@@ -4,6 +4,7 @@ import {ReactNode} from "react";
 import SectionLinkComponent from "@/chunk/sectionLinkComponent";
 import AnimatableComponent from "@/chunk/animatableComponent";
 import CompanysImage from "@/public/assets/img/img-companys.webp";
+import ProductComponent from "@/component/productComponent";
 
 // Creating And Exporting Second Section Component As Default
 export default function SecondSectionComponent():ReactNode {
@@ -23,34 +24,10 @@ export default function SecondSectionComponent():ReactNode {
                     <div className={'flex gap-[20px] mb-[30px]'}>
                         <div className={'section-title-holder'}><h6 className={'section-title'}>PRODUCTS</h6></div>
                         <div className={'grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:w-[80%] w-[90%] border border-white/30'}>
-                            <a href={'#'} className={'border border-white/30 p-[30px]'}>
-                                <div className={'mb-[200px]'}>
-                                    <h5 className={'text-white text-[32px] text-bold mb-[20px] truncate w-full'}>Suite</h5>
-                                    <p className={'text-white/80 text-[16px] text-light line-clamp-5'}>Whether you need a full scope project or just ad-hoc work, SVZ has you covered. Our end-to-end suite of agency services can be customized to fit your Enterprise needs, covering strategy, branding, design, Webflow development, and CMS training.</p>
-                                </div>
-                                <div className={'bg-blue-500 max-w-[100px] aspect-square rounded-full'} />
-                            </a>
-                            <a href={'#'} className={'border border-white/30 p-[30px]'}>
-                                <div className={'mb-[200px]'}>
-                                    <h5 className={'text-white text-[32px] text-bold mb-[20px] truncate w-full'}>Growth</h5>
-                                    <p className={'text-white/80 text-[16px] text-light line-clamp-5'}>We find new demand, measure the competition, and put together a scope for you to take action on. Then we grow results by piloting the new campaign in market, tracking it, and always optimizing.</p>
-                                </div>
-                                <div className={'bg-pink-500 max-w-[100px] aspect-square'} />
-                            </a>
-                            <a href={'#'} className={'border border-white/30 p-[30px]'}>
-                                <div className={'mb-[200px]'}>
-                                    <h5 className={'text-white text-[32px] text-bold mb-[20px] truncate w-full'}>Unlimited</h5>
-                                    <p className={'text-white/80 text-[16px] text-light line-clamp-5'}>Experience an all-inclusive, unlimited subscription service for top-tier design and Webflow development with our US Based Team. Our enterprise-grade quality is unmatched, with no room for gimmicks or subpar work.</p>
-                                </div>
-                                <div className={'bg-green-500 max-w-[100px] aspect-square rounded-full'} />
-                            </a>
-                            <a href={'#'} className={'border border-white/30 p-[30px] xl:col-span-1 lg:col-span-3 cols-span-1'}>
-                                <div className={'mb-[200px]'}>
-                                    <h5 className={'text-white text-[32px] text-bold mb-[20px] truncate w-full'}>Figma to Webflow</h5>
-                                    <p className={'text-white/80 text-[16px] text-light line-clamp-5'}>Whether we've just finished designing your new site or you're handing off finished designs for us to develop in Webflow, we're here to apply our trusted development process to your project.</p>
-                                </div>
-                                <div className={'bg-blue-500 max-w-[100px] aspect-square'} />
-                            </a>
+                            <ProductComponent title={'Suite'} icon={'circle'} iconColor={'bg-blue-500'}>Whether you need a full scope project or just ad-hoc work, SVZ has you covered. Our end-to-end suite of agency services can be customized to fit your Enterprise needs, covering strategy, branding, design, Webflow development, and CMS training.</ProductComponent>
+                            <ProductComponent title={'Growth'} icon={'square'} iconColor={'bg-pink-500'}>We find new demand, measure the competition, and put together a scope for you to take action on. Then we grow results by piloting the new campaign in market, tracking it, and always optimizing.</ProductComponent>
+                            <ProductComponent title={'Unlimited'} icon={'circle'} iconColor={'bg-green-500'}>Experience an all-inclusive, unlimited subscription service for top-tier design and Webflow development with our US Based Team. Our enterprise-grade quality is unmatched, with no room for gimmicks or subpar work.</ProductComponent>
+                            <ProductComponent title={'Figma to Webflow'} willScale icon={'square'} iconColor={'bg-blue-500'}>Whether we've just finished designing your new site or you're handing off finished designs for us to develop in Webflow, we're here to apply our trusted development process to your project.</ProductComponent>
                         </div>
                     </div>
                     <div className={'flex justify-end'}>
